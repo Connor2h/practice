f04ae6d82e3d2c2c367d3797a0b6760c06bc0504
Connor Hollis <connorhollis3d@gmail.com>
2022-04-01 23:33:06 -0600

    checkspeed

--- Practice-Problems/practice.js
+++ Practice-Problems/practice.js
@@ -12,11 +12,11 @@ let max = (num1, num2) => {
 let number = max(7, 2);
 console.log(number);
 
-let add = (num1,num2) => {
+let add = (num1, num2) => {
   return num1 + num2;
 }
 
-console.log(add(num1,num2));
+console.log(add(num1, num2));
 
 let width = 48;
 let height = 1024
@@ -31,26 +31,44 @@ let input = 7;
 
 console.log(fizzBuzz(input));
 
-function fizzBuzz(input){
-  if(typeof input !== 'number'){
-    return 'Not a number';
+function fizzBuzz(input) {
+  if (typeof input !== 'number') {
+    return NaN;
   }
 
-  if((input % 3 === 0) && (input % 5 === 0)){
+  if ((input % 3 === 0) && (input % 5 === 0)) {
     return 'fizzbuzz';
   }
 
-  
-  if(input % 5 === 0){
+
+  if (input % 5 === 0) {
     return 'buzz';
   }
-  
-  if(input % 5 === 0){
+
+  if (input % 5 === 0) {
     return 'fizz';
   }
-  
-  if((input % 3 !== 0) || (input % 5 !== 0)){
+
+  if ((input % 3 !== 0) || (input % 5 !== 0)) {
     return input;
   }
 
+}
+
+checkSpeed(129);
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint) {
+    return console.log("Ok");
+  }
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= 12) {
+    console.log('License suspended')
+  }
+  else {
+    console.log('Points: ', points);
+  }
 }
