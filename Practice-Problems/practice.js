c896fef42d066c5d0b8ab5012a89adb34d8a176e
Connor Hollis <connorhollis3d@gmail.com>
2022-04-03 20:08:19 -0600

    refactor show primes to be cleaner and break up concerns

--- Practice-Problems/practice.js
+++ Practice-Problems/practice.js
@@ -220,19 +220,40 @@
 
 // showStars(rows);
 
-showPrimes(10);
+// showPrimes(10);
+
+// function showPrimes(limit) {
+//   for(let number = 2; number <= limit; number++){
+//     let isPrime = true;
+//     for(let factor = 2; factor < number; factor++ ){
+//       if(number % factor === 0 ){
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if(isPrime){
+//       console.log(number);
+//     }
+//   }
+// }
+
+showPrimes(20);
 
 function showPrimes(limit) {
   for(let number = 2; number <= limit; number++){
-    let isPrime = true;
-    for(let factor = 2; factor < number; factor++ ){
-      if(number % factor === 0 ){
-        isPrime = false;
-        break;
-      }
-    }
-    if(isPrime){
+    if(isPrime(number)){
       console.log(number);
     }
   }
+}
+
+function isPrime(number){
+  let isPrime = true;
+  for(let factor = 2; factor < number; factor++ ){
+    if(number % factor === 0 ){
+      isPrime = false;
+      return false;
+    }
+  }
+  return true;
 }
